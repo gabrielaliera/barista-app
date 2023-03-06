1954bb8e59c031d1f887ee91581319f1ae07952f
gabrielaliera <gliera2990@gmail.com>
2023-03-06 10:22:25 -0800

    Add input state variable and ingredients dic

--- src/Components/baristaForm.jsx
+++ src/Components/baristaForm.jsx
@@ -1,9 +1,36 @@
 import REact, {Component, useState} from "react";
 
 const BarsitaForm = () =>{
+
+    const [input, setInputs] = useState({
+        'temperature': '',
+        'milk': '',
+        'syrup': '',
+        'blended': '' 
+    });
+
+    const ingredients = {
+        'temperature' : ['hot', 'lukewarm', 'cold'],
+      
+        'syrup': ['mocha', 'vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
+      
+        'milk': ['cow', 'oat', 'goat', 'almond', 'none'],
+      
+        'blended': ['yes', 'turbo', 'no']
+      }
+
+    const onCheckAnswer = () => {};
+
+    const onNewDrink = () => {};
     return(
         <div>
+            <h2>Hi, I'd like to order a:</h2>
+            <form>
 
+            </form>
+            <button type="submit" className="button submit" onClick={onCheckAnswer}>Check Answer</button>
+
+            <button type="new-drink-button" className=" button newdrink" onClick={onNewDrink}>New Drink</button>
         </div>
     )
 }
