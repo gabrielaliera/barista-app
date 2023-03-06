0ccac4dff5345889f0e48b445356d443e65f45b8
gabrielaliera <gliera2990@gmail.com>
2023-03-06 12:01:41 -0800

    Adjust CSS

--- src/Components/baristaForm.jsx
+++ src/Components/baristaForm.jsx
@@ -158,10 +158,6 @@ const BarsitaForm = () =>{
 
             </form>
             <button type="submit" className="button submit" onClick={onCheckAnswer}>Check Answer</button>
-
-            <button type="new-drink-button" className=" button newdrink" onClick={onNewDrink}>New Drink</button>
-
-           
         </div>
     )
 }
